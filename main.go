3af7b4a77980e76ecdf4cb63cb2af30db8ab9dc9
Tambua Evaristus <evaristustambua@gmail.com>
2023-07-27 13:45:12 +0100

    getbby id

--- main.go
+++ main.go
@@ -35,12 +35,24 @@ func addTodos(context *gin.Context){
 	context.IndentedJSON(http.StatusCreated, todos)
 }
 
+
+func getTodo(context *gin.Context){
+	id := context.Param("id")
+	todo, err := getTodoById(id)
+
+	if err != nil {
+		context.IndentedJSON(http.StatusNotFound, gin.H{"message" : "Todo not found"})
+		return
+	}
+ 	 context.IndentedJSON(http.StatusOK, todo)
+}
+
+
 func getTodoById(id string) (*todo, error) {
 	for i, t := range todos {
 		if t.ID == id {
 			return &todo[i], nil
 		}
-
 	}	
 
 	return nil, errors.New("todos not found")
@@ -51,5 +63,6 @@ func main()  {
 	router:= gin.Default()
 	router.GET("/todos", getTodos)
 	router.POST("/todos", addTodos)
+	router.GET("/todos/:id", getTodo)
 	router.Run("localhost:9090")
 }
